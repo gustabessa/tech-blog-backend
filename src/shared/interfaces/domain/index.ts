d07e275a8d78e9b0aab18a910ed20df76e02e5c4
Gustavo Bessa <gustavoux@yahoo.com.br>
2025-03-01 16:11:46 -0300

    refactor: remove incorrect export from domain interfaces

--- src/shared/interfaces/domain/index.ts
+++ src/shared/interfaces/domain/index.ts
@@ -1,5 +1,4 @@
 export * from './agregate-root.interface';
 export * from './identifier.interface';
 export * from './entity.interface';
-export * from './repository.interface';
 export * from './value-object.interface';
