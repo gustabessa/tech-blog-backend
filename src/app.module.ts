648a1a27e17d7dfa20edb378c240de9226c43d53
Gustavo Bessa <gustavoux@yahoo.com.br>
2025-03-02 13:17:11 -0300

    chore: provide interceptor that converts from result to http response

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,21 @@
 import { Module } from '@nestjs/common';
-import { SharedProvidersModule } from './shared/providers/shared-providers.module';
+import { APP_INTERCEPTOR } from '@nestjs/core';
 import { CoreInfrastructureModule } from './core/infrastructure/bootstrap/infrastructure.module';
+import { CoreUserModule } from './core/user/bootstrap/user.module';
+import { SharedProvidersModule } from './shared/providers/shared-providers.module';
+import { TransformResultToHttpResponseInterceptor } from './shared/utils/result/transform-result-to-http-response.interceptor';
 
 @Module({
   imports: [
     CoreInfrastructureModule.register(),
+    CoreUserModule.register(),
     SharedProvidersModule.register(),
   ],
-  controllers: [],
-  providers: [],
+  providers: [
+    {
+      provide: APP_INTERCEPTOR,
+      useClass: TransformResultToHttpResponseInterceptor,
+    },
+  ],
 })
 export class AppModule {}
