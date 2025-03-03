267b9361cdc3bc974553539f4f7aa29b83a0ffd5
Gustavo Bessa <gustavoux@yahoo.com.br>
2025-03-03 16:59:34 -0300

    chore: configure stricter project typings

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -27,9 +27,10 @@ export default tseslint.config(
   },
   {
     rules: {
-      '@typescript-eslint/no-explicit-any': 'off',
+      '@typescript-eslint/no-explicit-any': 'error',
       '@typescript-eslint/no-floating-promises': 'warn',
-      '@typescript-eslint/no-unsafe-argument': 'warn',
+      '@typescript-eslint/no-unsafe-argument': 'off',
+      '@typescript-eslint/no-unsafe-call': 'off',
       '@typescript-eslint/no-unused-vars': [
         'error',
         {
